0bf87cdbc9c658d354f09de363eabe6b898ccc40
Vachaspati Mishra <vachaspatimishraa@gmail.com>
2026-09-05 18:13:06 +0530

    resume parser

---
+++ test/unit/diagnose_vikash_test.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Inspect education and certifications section raw lines', () async {
+    // Let's print lines 63 to end
+  });
+}
